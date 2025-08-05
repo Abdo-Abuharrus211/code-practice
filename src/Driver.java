efe3fc11d954337065a834b9a3d9499f07392342
Abdo-Abuharrus211 <aabuharrus@gmail.com>
2025-08-04 23:36:41 -0700

    Update driver to test algo

--- src/Driver.java
+++ src/Driver.java
@@ -1,13 +1,8 @@
 public class Driver {
 
     public static void main(String[] args) {
-//        FloodFill floo = new FloodFill();
-//        int[][] image = {{1, 1, 1}, {1, 1, 0}, {1, 0, 1}};
-//        floo.floodFill(image, 1, 1, 2);
-        MedianTwoSortedArrays clssy = new MedianTwoSortedArrays();
-        int[] nums1 = {1, 3};
-        int[] nums2 = {2, 4};
-        System.out.println(clssy.findMedianSortedArrays(nums1, nums2));
+        FirstBadVersion firstBadVersion = new FirstBadVersion();
+        System.out.println(firstBadVersion.firstBadVersion(5));
 
     }
 }
